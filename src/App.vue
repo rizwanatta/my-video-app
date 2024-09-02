c9ee534042f61594b4cf8135a806a9c7fc59de46
rizwanatta <rizwanatta@protonmail.com>
2024-09-02 13:36:04 +0500

    feat: added two pages

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,5 @@
-<script setup>
-import HelloWorld from "./components/HelloWorld.vue";
-
-import { Button } from "@/components/ui/button";
-
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-</script>
+<script setup></script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
-
-  <Button>Click me</Button>
-
-  <Avatar class="h-52 w-52">
-    <AvatarImage src="https://github.com/shadcn.png" class="h-52 w-52" />
-    <AvatarFallback>CN</AvatarFallback>
-  </Avatar>
+  <router-view />
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
